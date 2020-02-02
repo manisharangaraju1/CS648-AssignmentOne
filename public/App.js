bfcc8cd235c91a1c5112df4f77a7d2796dc1a823
Manisha <manisha.rangaraju@gmail.com>
2020-02-02 13:06:47 -0800

    added css and changed introduction

--- public/App.js
+++ public/App.js
@@ -1,31 +1,31 @@
 var contentNode = document.getElementById('contents');
 
-const fullName = React.createElement(
-    'h1',
-    null,
-    'Manisha Rangaraju'
-);
-
-const headshot = React.createElement(
-    'div',
-    null,
-    ' ',
-    React.createElement('img', { src: './img/headshot.jpeg' }),
-    ' '
-);
-
-const introduction = React.createElement(
-    'p',
-    null,
-    'I am a gradute student at SDSU. I love playing sports and reading.'
-);
-
 const component = React.createElement(
-    'div',
-    { className: 'element' },
-    fullName,
-    headshot,
-    introduction
+  "div",
+  { className: "element" },
+  React.createElement(
+    "h1",
+    { className: "border" },
+    "Manisha Rangaraju"
+  ),
+  React.createElement("img", { src: "./img/headshot.jpeg" }),
+  React.createElement(
+    "p",
+    null,
+    "I am a gradute student at SDSU. I worked on few projects which invlove developing mobile applications. Full stack application development has always been on my to do list and Im glad I have enrolled in this course through which I hope to learn quite a number of things."
+  ),
+  "When Im not coding, I love playing any sport, reading and learning new skills.",
+  React.createElement("br", null),
+  React.createElement("br", null),
+  React.createElement(
+    "button",
+    { type: "button", className: "border" },
+    React.createElement(
+      "a",
+      { href: "www.github.com/manisharangaraju1" },
+      "VIEW MY GITHUB REPO"
+    )
+  )
 );
 
 ReactDOM.render(component, contentNode);
--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 var contentNode = document.getElementById('contents');
 
-const fullName = <h1>Manisha Rangaraju</h1>;
-
-const headshot = <div> <img src='./img/headshot.jpeg'/> </div>;
-
-const introduction = <p>I am a gradute student at SDSU. I love playing sports and reading.</p>;
-
-
 const component = <div className="element">
-    {fullName}
-    {headshot}
-    {introduction}</div>;
+  <h1 className="border">Manisha Rangaraju</h1>
+  <img src='./img/headshot.jpeg'/>
+  <p>I am a gradute student at SDSU. I worked on few projects which invlove developing mobile applications.
+       Full stack application development has always been on my to do list and Im glad I have enrolled in this course through which I hope to learn quite a number of things.</p>
+  When Im not coding, I love playing any sport, reading and learning new skills.
+
+  <br/><br/>
+  <button type="button" className="border"><a href="www.github.com/manisharangaraju1">VIEW MY GITHUB REPO</a></button>
+</div>;
 
 
 
